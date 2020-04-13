53e051a753c4a93d6d6ba2196ea0ac3a19d7c064
prathamesh <prathmesh6464@gmail.com>
2020-04-13 19:23:02 +0530

    MODIIFED : Changed indentation.

--- FundooNotes/src/main/java/com/bridz/model/UserDetails.java
+++ FundooNotes/src/main/java/com/bridz/model/UserDetails.java
@@ -6,52 +6,68 @@ public class UserDetails {
 	private String lastName;
 	private String userName;
 	private String password;
+	
 	/**
 	 * @return the firstName
 	 */
 	public synchronized final String getFirstName() {
+		
 		return firstName;
 	}
+	
 	/**
 	 * @param firstName the firstName to set
 	 */
 	public synchronized final void setFirstName(String firstName) {
+		
 		this.firstName = firstName;
 	}
+	
 	/**
 	 * @return the lastName
 	 */
 	public synchronized final String getLastName() {
+		
 		return lastName;
 	}
+	
 	/**
 	 * @param lastName the lastName to set
 	 */
 	public synchronized final void setLastName(String lastName) {
+		
 		this.lastName = lastName;
 	}
+	
 	/**
 	 * @return the userName
 	 */
 	public synchronized final String getUserName() {
+		
 		return userName;
 	}
+	
 	/**
 	 * @param userName the userName to set
 	 */
 	public synchronized final void setUserName(String userName) {
+		
 		this.userName = userName;
 	}
+	
 	/**
 	 * @return the password
 	 */
 	public synchronized final String getPassword() {
+		
 		return password;
 	}
+	
 	/**
 	 * @param password the password to set
 	 */
 	public synchronized final void setPassword(String password) {
+		
 		this.password = password;
 	}
 	
