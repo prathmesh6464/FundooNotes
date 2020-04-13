7e3c2a9ecc2a5e3be49f30588f86b422d5cb6089
prathamesh <prathmesh6464@gmail.com>
2020-04-13 10:30:43 +0530

    MODIFIED : Changed user login and registration control.

--- FundooNotes/src/main/java/com/bridz/controller/UserLoginControl.java
+++ FundooNotes/src/main/java/com/bridz/controller/UserLoginControl.java
@@ -1,26 +1,32 @@
 package com.bridz.controller;
+import com.bridz.model.*;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class UserLoginControl {
-
-	@PostMapping("/userLogin") 
-	ModelAndView userLogin() {
-	 
- 		ModelAndView mv = new ModelAndView("Login.jsp");
- 		mv.addObject("name","prathamesh");
-		return mv; 
-	}
 	
-	@PostMapping("/userRegistration") 
-	ModelAndView userRegistration() {
-	 
- 		ModelAndView mv = new ModelAndView("UserRegistration.jsp");
- 		mv.addObject("name","prathamesh");
-		return mv; 
+	
+	@GetMapping("/userLogin")
+	public ModelAndView userLogin(@RequestParam("userName") String userName, @RequestParam("password") String password) {
+
+		ModelAndView mv = new ModelAndView("Home.jsp");
+		mv.addObject("userName", userName);
+		mv.addObject("password", password);
+		return mv;
 	}
-}
+
+	@GetMapping("/userRegistration")
+	public ModelAndView userRegistration(UserDetails userData) {
+
+		ModelAndView mv = new ModelAndView("Home.jsp");
+		mv.addObject("userData", userData);
+		return mv;
+	}
+
+} 
