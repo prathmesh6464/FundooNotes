71177be18260dc2370292c59f22d5697a406cd77
prathamesh <prathmesh6464@gmail.com>
2020-04-14 14:33:11 +0530

    MODIFIED : Changed comments and indentation.

--- FundooNotes/src/main/java/com/bridz/model/LoginData.java
+++ FundooNotes/src/main/java/com/bridz/model/LoginData.java
@@ -1,39 +1,33 @@
 package com.bridz.model;
 
+
 public class LoginData {
-	
+
+	//Variables
 	private String userName;
 	private String password;
-	
-	/**
-	 * @return the userName
-	 */
+
+	//@return the userName	
 	public String getUserName() {
-		
+
 		return userName;
 	}
-	
-	/**
-	 * @param userName the userName to set
-	 */
+
+	//@param userName the userName to set
 	public void setUserName(String userName) {
-		
+
 		this.userName = userName;
 	}
-	
-	/**
-	 * @return the password
-	 */
+
+	//@return the password	 
 	public String getPassword() {
-		
+
 		return password;
 	}
-	
-	/**
-	 * @param password the password to set
-	 */
+
+	//@param password the password to set
 	public void setPassword(String password) {
-		
+
 		this.password = password;
 	}	
 }
