8faf3327e90bab50c333080ce6cc584227ecd454
prathamesh <prathmesh6464@gmail.com>
2020-04-14 14:35:49 +0530

    MODIFIED : Added comments, login feature.

--- FundooNotes/src/main/java/com/bridz/controller/UserLoginControl.java
+++ FundooNotes/src/main/java/com/bridz/controller/UserLoginControl.java
@@ -1,26 +1,37 @@
 package com.bridz.controller;
-import com.bridz.model.*;
-import com.bridz.repository.*;
-
+import com.bridz.model.UserDetails;
+import com.bridz.model.LoginData;
+import com.bridz.repository.UserRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class UserLoginControl {
 	
+	//Created object of user repository
 	@Autowired
 	UserRepository userRepository;
 	
 	@GetMapping("/userLogin")
-	public  LoginData userLogin(LoginData loginData) {
-
-		return loginData;
+	public  List<UserDetails> userLogin(LoginData loginData) {
+	
+		//Checking user name and password is valid or not
+		if (userRepository.findByUserName(loginData.getUserName()).equals
+				(userRepository.findByPassword(loginData.getPassword()))) {
+			
+			return userRepository.findByPassword(loginData.getPassword());
+		}
+		
+		return null;
 	}
 
 	@GetMapping("/userRegistration")
 	public String userRegistration(UserDetails userData) {
 
+		//Storing user data into data base
 		userRepository.save(userData);
 		return "Successfully uploaded";
 	}
