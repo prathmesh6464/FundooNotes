a09fbb767e5a9c6c1ae956a5a784dfa468cddc3d
prathamesh <prathmesh6464@gmail.com>
2020-04-14 14:29:46 +0530

    MODIFIED : Changed comment style and indentation.

--- FundooNotes/src/main/java/com/bridz/model/UserDetails.java
+++ FundooNotes/src/main/java/com/bridz/model/UserDetails.java
@@ -2,74 +2,60 @@ package com.bridz.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+
 @Entity
 public class UserDetails {
 
+	//Variables
 	@Id
 	private String firstName;
 	private String lastName;
 	private String userName;
 	private String password;
 
-	/**
-	 * @return the firstName
-	 */
+	//@return the firstName	 
 	public String getFirstName() {
 
 		return firstName;
 	}
 
-	/**
-	 * @param firstName the firstName to set
-	 */
+	//@param firstName the firstName to set
 	public void setFirstName(String firstName) {
 
 		this.firstName = firstName;
 	}
 
-	/**
-	 * @return the lastName
-	 */
+	//@return the lastName
 	public String getLastName() {
 
 		return lastName;
 	}
 
-	/**
-	 * @param lastName the lastName to set
-	 */
+	//@param lastName the lastName to set
 	public void setLastName(String lastName) {
 
 		this.lastName = lastName;
 	}
 
-	/**
-	 * @return the userName
-	 */
+	//@return the userName
 	public String getUserName() {
 
 		return userName;
 	}
 
-	/**
-	 * @param userName the userName to set
-	 */
+	//@param userName the userName to set
 	public void setUserName(String userName) {
 
 		this.userName = userName;
 	}
 
-	/**
-	 * @return the password
-	 */
+	//@return the password
 	public String getPassword() {
 
 		return password;
 	}
 
-	/**
-	 * @param password the password to set
-	 */
+	//@param password the password to set
 	public void setPassword(String password) {
 
 		this.password = password;
