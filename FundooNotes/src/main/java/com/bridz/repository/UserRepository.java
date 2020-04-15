6f54fad88b1f64604cfc1d42f7a99cd5865b279c
prathamesh <prathmesh6464@gmail.com>
2020-04-15 09:31:55 +0530

    REFACTORED : Added update query for updating password.

--- FundooNotes/src/main/java/com/bridz/repository/UserRepository.java
+++ FundooNotes/src/main/java/com/bridz/repository/UserRepository.java
@@ -1,15 +1,24 @@
 package com.bridz.repository;
+
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import com.bridz.model.UserDetails;
 
-
 //Interface
-public interface UserRepository extends JpaRepository<UserDetails, Integer>  {
-	
-	//Method for finding data by using user name and password
+public interface UserRepository extends JpaRepository<UserDetails, Integer> {
+
+	// Method for finding data by using user name and password
 	List<UserDetails> findByUserName(String userName);
+
 	List<UserDetails> findByPassword(String password);
+
 	List<UserDetails> findBySecretEmergencyWord(String secretEmergencyWord);
+
 	List<UserDetails> findByFirstMobileNumber(long firstMobileNumber);
+
+	@Query(value = "UPDATE user_details SET password = ? WHERE secret_emergency_word = ?", nativeQuery = true)
+	@Modifying
+	List<UserDetails> updateUserDetails(String resetPassword, String secretEmergencyWord);
 }
