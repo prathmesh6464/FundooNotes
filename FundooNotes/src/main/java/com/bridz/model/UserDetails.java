9d0bd16ff080c7a115a22f48e709796403a9539e
prathamesh <prathmesh6464@gmail.com>
2020-04-15 09:25:12 +0530

    MODIFIED : Changed indentation.

--- FundooNotes/src/main/java/com/bridz/model/UserDetails.java
+++ FundooNotes/src/main/java/com/bridz/model/UserDetails.java
@@ -1,12 +1,12 @@
 package com.bridz.model;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
 
 @Entity
 public class UserDetails {
 
-	//Variables
+	// Variables
 	@Id
 	private String firstName;
 	private String lastName;
@@ -16,88 +16,88 @@ public class UserDetails {
 	private String secretEmergencyWord;
 	private long firstMobileNumber;
 
-	//@return the firstName	 
+	// @return the firstName
 	public String getFirstName() {
 
 		return firstName;
 	}
 
-	//@param firstName the firstName to set
+	// @param firstName the firstName to set
 	public void setFirstName(String firstName) {
 
 		this.firstName = firstName;
 	}
 
-	//@return the lastName
+	// @return the lastName
 	public String getLastName() {
 
 		return lastName;
 	}
 
-	//@param lastName the lastName to set
+	// @param lastName the lastName to set
 	public void setLastName(String lastName) {
 
 		this.lastName = lastName;
 	}
 
-	//@return the userName
+	// @return the userName
 	public String getUserName() {
 
 		return userName;
 	}
 
-	//@param userName the userName to set
+	// @param userName the userName to set
 	public void setUserName(String userName) {
 
 		this.userName = userName;
 	}
 
-	//@return the password
+	// @return the password
 	public String getPassword() {
 
 		return password;
 	}
 
-	//@param password the password to set
+	// @param password the password to set
 	public void setPassword(String password) {
 
 		this.password = password;
 	}
 
-	//@return the mobileNumber
+	// @return the mobileNumber
 	public long getMobileNumber() {
-		
+
 		return mobileNumber;
 	}
 
-	//@param mobileNumber the mobileNumber to set
+	// @param mobileNumber the mobileNumber to set
 	public void setMobileNumber(long mobileNumber) {
-		
+
 		this.mobileNumber = mobileNumber;
 	}
 
-	//@return the firstMobileNumber
+	// @return the firstMobileNumber
 	public long getFirstMobileNumber() {
-		
+
 		return firstMobileNumber;
 	}
 
-	//@param firstMobileNumber the firstMobileNumber to set
+	// @param firstMobileNumber the firstMobileNumber to set
 	public void setFirstMobileNumber(long firstMobileNumber) {
 
 		this.firstMobileNumber = firstMobileNumber;
 	}
-	
-	//@return the secretEmergencyWord
-	
+
+	// @return the secretEmergencyWord
+
 	public String getSecretEmergencyWord() {
-		
+
 		return secretEmergencyWord;
 	}
 
-	//@param secretEmergencyWord the secretEmergencyWord to set
+	// @param secretEmergencyWord the secretEmergencyWord to set
 	public void setSecretEmergencyWord(String secretEmergencyWord) {
-		
+
 		this.secretEmergencyWord = secretEmergencyWord;
 	}
 }
