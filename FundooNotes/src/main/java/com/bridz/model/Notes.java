51192c693dcab56705921fb9fc9cee56b27c8264
prathamesh <prathmesh6464@gmail.com>
2020-04-16 14:12:33 +0530

    ADDED : Notes entity class to create notes table.

---
+++ FundooNotes/src/main/java/com/bridz/model/Notes.java
@@ -0,0 +1,55 @@
+package com.bridz.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Notes {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long noteId;
+	private String message;
+	@ManyToOne
+	private UserDetails userDetails;
+
+	// @return the noteId
+	public long getNoteId() {
+
+		return noteId;
+	}
+
+	// @param noteId the noteId to set
+	public void setNoteId(long noteId) {
+
+		this.noteId = noteId;
+	}
+
+	// @return the message
+	public String getMessage() {
+
+		return message;
+	}
+
+	// @param message the message to set
+	public void setMessage(String message) {
+
+		this.message = message;
+	}
+
+	// @return the userDetails
+	public UserDetails getUserDetails() {
+
+		return userDetails;
+	}
+
+	// @param userDetails the userDetails to set
+	public void setUserDetails(UserDetails userDetails) {
+
+		this.userDetails = userDetails;
+	}
+
+}
