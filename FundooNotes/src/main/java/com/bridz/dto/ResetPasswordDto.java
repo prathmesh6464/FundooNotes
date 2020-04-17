27f3d55dbaf8c5abe06c2754a277b52fd6e0f7cd
prathamesh <prathmesh6464@gmail.com>
2020-04-17 11:39:05 +0530

    ADDED : Reset password dto.

---
+++ FundooNotes/src/main/java/com/bridz/dto/ResetPasswordDto.java
@@ -0,0 +1,31 @@
+package com.bridz.dto;
+
+public class ResetPasswordDto {
+
+	private String password;
+	private String confirmPassword;
+
+	// @return the password
+	public String getPassword() {
+
+		return password;
+	}
+
+	// @param password the password to set
+	public void setPassword(String password) {
+
+		this.password = password;
+	}
+
+	// @return the confirmPassword
+	public String getConfirmPassword() {
+
+		return confirmPassword;
+	}
+
+	// @param confirmPassword the confirmPassword to set
+	public void setConfirmPassword(String confirmPassword) {
+
+		this.confirmPassword = confirmPassword;
+	}
+}
