560941f52076bfe72a14da25e0f8816956eed988
prathamesh <prathmesh6464@gmail.com>
2020-04-17 11:43:09 +0530

    ADDED : Data transfer object of secret information for forget password.

---
+++ FundooNotes/src/main/java/com/bridz/dto/SecretInformationDto.java
@@ -0,0 +1,32 @@
+package com.bridz.dto;
+
+public class SecretInformationDto {
+
+	// Variables
+	private String secretEmergencyWord;
+	private long firstMobileNumber;
+
+	// @return the secretEmergencyWord
+	public String getSecretEmergencyWord() {
+
+		return secretEmergencyWord;
+	}
+
+	// @param secretEmergencyWord the secretEmergencyWord to set
+	public void setSecretEmergencyWord(String secretEmergencyWord) {
+
+		this.secretEmergencyWord = secretEmergencyWord;
+	}
+
+	// @return the firstMobileNumber
+	public long getFirstMobileNumber() {
+
+		return firstMobileNumber;
+	}
+
+	// @param firstMobileNumber the firstMobileNumber to set
+	public void setFirstMobileNumber(long firstMobileNumber) {
+
+		this.firstMobileNumber = firstMobileNumber;
+	}
+}
