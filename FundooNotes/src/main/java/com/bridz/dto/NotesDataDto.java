9a266ad3cd8ab8bf26f9c8ac19feff8f7f395971
prathamesh <prathmesh6464@gmail.com>
2020-04-17 11:35:58 +0530

    ADDED : notes data dto.

---
+++ FundooNotes/src/main/java/com/bridz/dto/NotesDataDto.java
@@ -0,0 +1,37 @@
+package com.bridz.dto;
+
+import javax.persistence.ManyToOne;
+import com.bridz.model.UserDetails;
+
+public class NotesDataDto {
+
+	private String message;
+
+	@ManyToOne
+	private UserDetails userDetails;
+
+	// @return the message
+	public String getMessage() {
+
+		return message;
+	}
+
+	// @param message the message to set
+	public void setMessage(String message) {
+
+		this.message = message;
+	}
+
+	// @return the userDetails
+	public UserDetails getUserDetails() {
+
+		return userDetails;
+	}
+
+	// @param userDetails the userDetails to set
+	public void setUserDetails(UserDetails userDetails) {
+
+		this.userDetails = userDetails;
+	}
+
+}
