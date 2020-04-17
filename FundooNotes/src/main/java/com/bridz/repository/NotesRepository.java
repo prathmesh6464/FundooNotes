6ec499033098d5650ec3a77ae77de8f63a514e1d
prathamesh <prathmesh6464@gmail.com>
2020-04-17 12:16:36 +0530

    MODIFIED : change object type notes to notes data.

--- FundooNotes/src/main/java/com/bridz/repository/NotesRepository.java
+++ FundooNotes/src/main/java/com/bridz/repository/NotesRepository.java
@@ -1,8 +1,9 @@
 package com.bridz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.bridz.model.Notes;
 
-public interface NotesRepository extends JpaRepository<Notes, Integer> {
+import com.bridz.model.NotesData;
+
+public interface NotesRepository extends JpaRepository<NotesData, Integer> {
 
 }
