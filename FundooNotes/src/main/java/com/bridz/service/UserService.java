46820ad2a65d7512d8e8db52e007af7218fee77f
prathamesh <prathmesh6464@gmail.com>
2020-04-18 20:43:41 +0530

    REFACTORED : Renamed login data dto, reset password data dto, secret information and changed package from dto to model.

--- FundooNotes/src/main/java/com/bridz/service/UserService.java
+++ FundooNotes/src/main/java/com/bridz/service/UserService.java
@@ -1,17 +1,17 @@
 package com.bridz.service;
 
 import java.util.List;
-import com.bridz.dto.LoginDataDto;
-import com.bridz.dto.ResetPasswordDto;
-import com.bridz.dto.SecretInformationDto;
+import com.bridz.model.LoginData;
+import com.bridz.model.ResetPasswordData;
+import com.bridz.model.SecretInformation;
 import com.bridz.model.UserDetails;
 import com.bridz.response.Response;
 
 public interface UserService {
 	
 	public Response registerUser(UserDetails userDetails);
-	public List<UserDetails> userLogin(LoginDataDto  userLoginDtoObject);
-	public Response forgetPassword(SecretInformationDto secretInformationData);
-	public Response resetPassword(ResetPasswordDto resetPasswordDto, String secretWord);
+	public List<UserDetails> userLogin(LoginData  userLoginDtoObject);
+	public Response forgetPassword(SecretInformation secretInformationData);
+	public Response resetPassword(ResetPasswordData resetPasswordDto, String secretWord);
 	
 }
