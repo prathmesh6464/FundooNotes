1ddec6a5ebbd5ee7c85b813fc98b78a6597b7700
prathamesh <prathmesh6464@gmail.com>
2020-04-18 10:33:40 +0530

    WIP : Used service class and dto class in every method of user login control.

--- FundooNotes/src/main/java/com/bridz/controller/UserLoginControl.java
+++ FundooNotes/src/main/java/com/bridz/controller/UserLoginControl.java
@@ -4,6 +4,7 @@ import com.bridz.model.UserDetails;
 import com.bridz.dto.LoginDataDto;
 import com.bridz.dto.ResetPasswordDto;
 import com.bridz.dto.SecretInformationDto;
+import com.bridz.dto.UserDetailsDto;
 import com.bridz.model.NotesData;
 import com.bridz.repository.NotesRepository;
 import com.bridz.repository.UserRepository;
@@ -11,6 +12,7 @@ import com.bridz.response.Response;
 import com.bridz.service.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,84 +25,53 @@ public class UserLoginControl {
 	@Autowired
 	UserService userServiceObject;
 
-	// Created object of user repository
-	@Autowired
-	UserRepository userRepository;
-
-	@Autowired
-	NotesRepository notesRepository;
-
 	String secretWord;
 
 	@GetMapping("/userLogin")
-	public List<UserDetails> userLogin(LoginDataDto loginDataDto) {
+	public List<UserDetails> userLogin(LoginDataDto userLoginDtoObject) {
 
-		// Checking user name and password is valid or not
-		if (userRepository.findByUserName(loginDataDto.getUserName())
-				.equals(userRepository.findByPassword(loginDataDto.getPassword()))) {
-
-			return userRepository.findByPassword(loginDataDto.getPassword());
-		}
-
-		return null;
+		return userServiceObject.userLogin(userLoginDtoObject);
 	}
 
 	@RequestMapping(value = "/userRegistration", method = { RequestMethod.POST })
-	public Response userRegistration(@RequestParam("firstName") String firstName,
-			@RequestParam("lastName") String lastName, @RequestParam("userName") String userName,
-			@RequestParam("password") String password, @RequestParam("mobileNumber") long mobileNumber,
-			@RequestParam("secretEmergencyWord") String secretEmergencyWord,
-			@RequestParam("firstMobileNumber") long firstMobileNumber) {
+	public Response userRegistration(UserDetailsDto userDetailsDtoObject) {
 
-		UserDetails userDetailsObject = new UserDetails(firstName, lastName, userName, password, mobileNumber,
-				secretEmergencyWord, firstMobileNumber);
+		UserDetails userDetailsObject = new UserDetails(userDetailsDtoObject);
 		
 		// Storing user data into data base
-		userServiceObject.registerUser(userDetailsObject);
-		return new Response(200, "Successfully uploaded");
+		Response response = userServiceObject.registerUser(userDetailsObject);
+		
+		return response;
 	}
 
 	@GetMapping("/forgetPassword")
-	public boolean forgetPassword(SecretInformationDto secretInformationData) {
+	public Response forgetPassword(SecretInformationDto secretInformationData) {
 
 		secretWord = secretInformationData.getSecretEmergencyWord();
-		// Checking secret information is valid or not
-		if (userRepository.findBySecretEmergencyWord(secretInformationData.getSecretEmergencyWord())
-				.equals(userRepository.findByFirstMobileNumber(secretInformationData.getFirstMobileNumber())))
-			return true;
-
-		return false;
+		Response response = userServiceObject.forgetPassword(secretInformationData);	
+		
+		return response;
 	}
 
 	@RequestMapping(value = "/resetPassword", method = { RequestMethod.PUT })
-	public String resetPassword(ResetPasswordDto resetPasswordDto) {
-
-		// Storing user's edited information to data base
-		if (resetPasswordDto.getPassword().equals(resetPasswordDto.getConfirmPassword())) {
-
-			try {
-				userRepository.setPassword(resetPasswordDto.getPassword(), secretWord);
-			} catch (Exception e) {
-				return "Successfully password changed";
-			}
-		}
-
-		return "Confirm password and password not matched";
+	public Response resetPassword(ResetPasswordDto resetPasswordDto) {
+		
+		return userServiceObject.resetPassword(resetPasswordDto, secretWord);	
 	}
 
-	@RequestMapping(value = "/addNote", method = { RequestMethod.POST })
-	public String addUserNote(NotesData userNote) {
-
-		// Storing user note data into data base
-		notesRepository.save(userNote);
-		return "Note added successfully";
-	}
-
-	@RequestMapping(value = "/deleteNote", method = { RequestMethod.DELETE })
-	public String deleteUserNote(NotesData userNote) {
-
-		// Storing user note data into data base
-		// notesRepository.delete();
-		return "Note deleted successfully";
-	}
+//	@RequestMapping(value = "/addNote", method = { RequestMethod.POST })
+//	public String addUserNote(NotesData userNote) {
+//
+//		// Storing user note data into data base
+//		notesRepository.save(userNote);
+//		return "Note added successfully";
+//	}
+//
+//	@RequestMapping(value = "/deleteNote", method = { RequestMethod.DELETE })
+//	public String deleteUserNote(NotesData userNote) {
+//
+//		// Storing user note data into data base
+//		// notesRepository.delete();
+//		return "Note deleted successfully";
+//	}
 }
