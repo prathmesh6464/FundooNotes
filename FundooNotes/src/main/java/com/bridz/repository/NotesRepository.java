ff041e9ca163341d68a41378caffafd1d36711bc
prathamesh <prathmesh6464@gmail.com>
2020-04-18 17:10:50 +0530

    MODIFIED : Changed indentation.

--- FundooNotes/src/main/java/com/bridz/repository/NotesRepository.java
+++ FundooNotes/src/main/java/com/bridz/repository/NotesRepository.java
@@ -1,7 +1,6 @@
 package com.bridz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.bridz.model.NotesData;
 
 public interface NotesRepository extends JpaRepository<NotesData, Integer> {
