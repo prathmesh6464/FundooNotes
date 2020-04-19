7c32bfc398435b7ff23241d85be85948e75ada65
prathamesh <prathmesh6464@gmail.com>
2020-04-19 12:16:04 +0530

    ADDED : User registration dto for saving user's data into database

---
+++ FundooNotes/src/main/java/com/bridz/dto/UserRegistrationDto.java
@@ -0,0 +1,120 @@
+package com.bridz.dto;
+
+
+public class UserRegistrationDto {
+
+	// Variables
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private long mobileNumber;
+	private String secretEmergencyWord;
+	private long firstMobileNumber;
+	
+	//Default constructor
+	public UserRegistrationDto() {
+		
+		super();
+	}
+
+	//Parameterized constructor
+	public UserRegistrationDto(String firstName, String lastName, String userName, String password, long mobileNumber, 
+		String secretWord, long firstMobileNumber) {
+
+		super(); 
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+		this.mobileNumber = mobileNumber;
+		this.secretEmergencyWord = secretWord;
+		this.firstMobileNumber = firstMobileNumber;
+	}
+
+	// @return the firstName
+	public String getFirstName() {
+
+		return firstName;
+	}
+
+	// @param firstName the firstName to set
+	public void setFirstName(String firstName) {
+
+		this.firstName = firstName;
+	}
+
+	// @return the lastName
+	public String getLastName() {
+
+		return lastName;
+	}
+
+	// @param lastName the lastName to set
+	public void setLastName(String lastName) {
+
+		this.lastName = lastName;
+	}
+
+	// @return the userName
+	public String getUserName() {
+
+		return userName;
+	}
+
+	// @param userName the userName to set
+	public void setUserName(String userName) {
+
+		this.userName = userName;
+	}
+
+	// @return the password
+	public String getPassword() {
+
+		return password;
+	}
+
+	// @param password the password to set
+	public void setPassword(String password) {
+
+		this.password = password;
+	}
+
+	// @return the mobileNumber
+	public long getMobileNumber() {
+
+		return mobileNumber;
+	}
+
+	// @param mobileNumber the mobileNumber to set
+	public void setMobileNumber(long mobileNumber) {
+
+		this.mobileNumber = mobileNumber;
+	}
+
+	// @return the firstMobileNumber
+	public long getFirstMobileNumber() {
+
+		return firstMobileNumber;
+	}
+
+	// @param firstMobileNumber the firstMobileNumber to set
+	public void setFirstMobileNumber(long firstMobileNumber) {
+
+		this.firstMobileNumber = firstMobileNumber;
+	}
+
+	// @return the secretEmergencyWord
+
+	public String getSecretEmergencyWord() {
+
+		return secretEmergencyWord;
+	}
+
+	// @param secretEmergencyWord the secretEmergencyWord to set
+	public void setSecretEmergencyWord(String secretEmergencyWord) {
+
+		this.secretEmergencyWord = secretEmergencyWord;
+	}
+}
+
