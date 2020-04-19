3cd62082b528368814dc2554b66d50d0d133f5b3
prathamesh <prathmesh6464@gmail.com>
2020-04-19 08:13:19 +0530

    REFATORED : Removed variables password, mobile number, secretWord, first mobile number.

--- FundooNotes/src/main/java/com/bridz/dto/UserDetailsDto.java
+++ FundooNotes/src/main/java/com/bridz/dto/UserDetailsDto.java
@@ -3,7 +3,6 @@ package com.bridz.dto;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.OneToMany;
-import com.bridz.model.NotesData;
 
 public class UserDetailsDto {
 
@@ -11,11 +10,7 @@ public class UserDetailsDto {
 	private String firstName;
 	private String lastName;
 	private String userName;
-	private String password;
-	private long mobileNumber;
-	private String secretEmergencyWord;
-	private long firstMobileNumber;
-
+	
 	@OneToMany(mappedBy = "userDetails")
 	private List<NotesDataDto> notesDataDto = new ArrayList<>();
 
@@ -53,55 +48,6 @@ public class UserDetailsDto {
 	public void setUserName(String userName) {
 
 		this.userName = userName;
-	}
-
-	// @return the password
-	public String getPassword() {
-
-		return password;
-	}
-
-	// @param password the password to set
-	public void setPassword(String password) {
-
-		this.password = password;
-	}
-
-	// @return the mobileNumber
-	public long getMobileNumber() {
-
-		return mobileNumber;
-	}
-
-	// @param mobileNumber the mobileNumber to set
-	public void setMobileNumber(long mobileNumber) {
-
-		this.mobileNumber = mobileNumber;
-	}
-
-	// @return the firstMobileNumber
-	public long getFirstMobileNumber() {
-
-		return firstMobileNumber;
-	}
-
-	// @param firstMobileNumber the firstMobileNumber to set
-	public void setFirstMobileNumber(long firstMobileNumber) {
-
-		this.firstMobileNumber = firstMobileNumber;
-	}
-
-	// @return the secretEmergencyWord
-
-	public String getSecretEmergencyWord() {
-
-		return secretEmergencyWord;
-	}
-
-	// @param secretEmergencyWord the secretEmergencyWord to set
-	public void setSecretEmergencyWord(String secretEmergencyWord) {
-
-		this.secretEmergencyWord = secretEmergencyWord;
 	}
 
 	// @return the notesDataDto
