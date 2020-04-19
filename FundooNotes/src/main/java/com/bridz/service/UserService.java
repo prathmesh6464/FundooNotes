a572b4e7f5df9efb9bc3be824c251a5f8d36578f
prathamesh <prathmesh6464@gmail.com>
2020-04-19 12:11:00 +0530

    MODIFIED : Changed register user method's parameter.

--- FundooNotes/src/main/java/com/bridz/service/UserService.java
+++ FundooNotes/src/main/java/com/bridz/service/UserService.java
@@ -6,12 +6,13 @@ import com.bridz.dto.LoginDto;
 import com.bridz.dto.ResetPasswordDto;
 import com.bridz.dto.SecretInformationDto;
 import com.bridz.dto.UserDetailsDto;
+import com.bridz.dto.UserRegistrationDto;
 import com.bridz.model.UserDetails;
 import com.bridz.response.Response;
 
 public interface UserService {
 
-	public Response registerUser(UserDetails userDetails);
+	public Response registerUser(UserRegistrationDto userRegisterDtoObject);
 
 	public List<UserDetailsDto> userLogin(LoginDto userLoginDtoObject);
 
