c059e5579f1a281e92347e12a465044d405d6c58
prathamesh <prathmesh6464@gmail.com>
2020-04-20 10:40:05 +0530

    MODIFIED : Added validation on variables.

--- FundooNotes/src/main/java/com/bridz/dto/SecretInformationDto.java
+++ FundooNotes/src/main/java/com/bridz/dto/SecretInformationDto.java
@@ -1,9 +1,18 @@
 package com.bridz.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class SecretInformationDto {
 
 	// Variables
+	@NotNull
 	private String secretEmergencyWord;
+
+	@NotNull
+	@Size(min = 10, max = 10)
+	@Pattern(regexp = "[0-9]*")
 	private long firstMobileNumber;
 
 	// @return the secretEmergencyWord
