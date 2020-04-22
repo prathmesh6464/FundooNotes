45017e3aaf6d93e9913fcf32dd82136ded2462c0
prathamesh <prathmesh6464@gmail.com>
2020-04-22 15:35:11 +0530

    MODIFIED : Changed indentation.

--- FundooNotes/src/main/java/com/bridz/service/JwtService.java
+++ FundooNotes/src/main/java/com/bridz/service/JwtService.java
@@ -4,8 +4,9 @@ import java.util.Map;
 import com.bridz.dto.ForgetPasswordDto;
 
 public interface JwtService {
-	
+
 	public String generateToken(ForgetPasswordDto forgetPasswordDtoObject);
+
 	public String createToken(Map<String, Object> claims, String subject);
 
 }
