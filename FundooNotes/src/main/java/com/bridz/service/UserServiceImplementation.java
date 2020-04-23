933621ef02371bb42452d451acec8f221d010960
prathamesh <prathmesh6464@gmail.com>
2020-04-23 17:50:45 +0530

    REFACTORED : Added user verification method and changed register user method.

--- FundooNotes/src/main/java/com/bridz/service/UserServiceImplementation.java
+++ FundooNotes/src/main/java/com/bridz/service/UserServiceImplementation.java
@@ -35,6 +35,12 @@ public class UserServiceImplementation implements UserService {
 	@Autowired
 	EmailService emailServiceObject;
 
+	// Token variable for verification of token
+	String token;
+
+	// User registration dto object used in user verification
+	UserRegistrationDto userRegistrationObject;
+
 	// Constructor
 	public UserServiceImplementation(UserRepository userRepository) {
 
@@ -43,13 +49,23 @@ public class UserServiceImplementation implements UserService {
 	}
 
 	@Override
-	public Response registerUser(UserRegistrationDto userRegistrationDtoObject) {
+	public Response registerUser(UserRegistrationDto userRegisterDtoObject) {
 
-		// Using model mapper mapping dto object with user details entity
-		modelMapperObject.map(userRegistrationDtoObject, userDetailsObject);
-		userRepository.save(userDetailsObject);
+		// User registration object used in user Verification
+		userRegistrationObject = userRegisterDtoObject;
 
-		return new Response("User details saved", 200);
+		// Email related variables
+		String to = "requestchecking@gmail.com";
+		String subject = "Authentication";
+		token = jwtTokenObject.generateToken(userRegisterDtoObject);
+
+		// Reset url
+		String resetPasswordUrl = "http://localhost:8081/userVerification/" + token;
+
+		// Send email method callled
+		emailServiceObject.send(to, subject, resetPasswordUrl);
+
+		return new Response("User authentication started", 200);
 	}
 
 	@Override
@@ -104,6 +120,23 @@ public class UserServiceImplementation implements UserService {
 		}
 
 		return new Response("Confirm password and password not matched", 201);
+	}
+
+	@Override
+	public Response userVerification(String emailToken) {
+
+		// Using model mapper mapping dto object with user details entity
+		modelMapperObject.map(userRegistrationObject, userDetailsObject);
+		
+		if (emailToken.equals(token)) {
+
+			// saving user data into database
+			userRepository.save(userDetailsObject);
+
+			return new Response("User varification completed and user registered", 200);
+		}
+
+		return new Response("User varification failed", 201);
 	}
 
 }
