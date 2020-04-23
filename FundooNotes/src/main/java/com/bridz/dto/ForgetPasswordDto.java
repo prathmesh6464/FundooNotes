2d6609bdf6feecfa5004f67a88d7065bcdd9083b
prathamesh <prathmesh6464@gmail.com>
2020-04-23 23:22:14 +0530

    MODIFIED : Added pattern for email_id.

--- FundooNotes/src/main/java/com/bridz/dto/ForgetPasswordDto.java
+++ FundooNotes/src/main/java/com/bridz/dto/ForgetPasswordDto.java
@@ -2,12 +2,14 @@ package com.bridz.dto;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 public class ForgetPasswordDto {
 
 	// Variables
 	@NotEmpty
 	@NotNull
+	@Pattern(regexp = "[a-zA-Z0-9]*@[a-zA-Z]+[.][a-zA-Z]+")
 	private String emailId;
 
 	// @return the emailId
