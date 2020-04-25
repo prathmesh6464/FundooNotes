078bfcbacf99686fb0b8c358e2dab2ef3bef337a
prathamesh <prathmesh6464@gmail.com>
2020-04-26 00:48:14 +0530

    REFACTORED : Added show user notes, update user note methods and changed add user note, delete user note methods.

--- FundooNotes/src/main/java/com/bridz/controller/NotesController.java
+++ FundooNotes/src/main/java/com/bridz/controller/NotesController.java
@@ -1,13 +1,17 @@
 package com.bridz.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.bridz.dto.NotesDto;
+import com.bridz.model.NotesData;
 import com.bridz.service.NotesService;
 
 @RestController
@@ -28,6 +32,21 @@ public class NotesController {
 
 		// Deleting user note from data base
 		return notesServiceObject.deleteNote(title);
+	}
+
+	@PutMapping("/updateNote/{title}")
+	public ResponseEntity<String> updateUserNote(@PathVariable("title") String title,
+			@RequestBody NotesDto notesDtoObject) {
+
+		// Storing user note data into data base
+		return notesServiceObject.updateNote(notesDtoObject, title);
+	}
+
+	@GetMapping("/showNotes")
+	public List<NotesData> showeUserNotes() {
+
+		// Deleting user note from data base
+		return notesServiceObject.showNotes();
 	}
 
 }
