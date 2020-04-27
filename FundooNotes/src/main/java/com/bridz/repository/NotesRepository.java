7c94092d5b5479caaa52bede9eb4856027ec9f7a
prathamesh <prathmesh6464@gmail.com>
2020-04-27 14:00:43 +0530

    REFACTORED : Added find by description, find by title, sort by description, sort by title, set archive, set pinded, set trash, set title Description Queries.

--- FundooNotes/src/main/java/com/bridz/repository/NotesRepository.java
+++ FundooNotes/src/main/java/com/bridz/repository/NotesRepository.java
@@ -1,16 +1,39 @@
 package com.bridz.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.bridz.model.NotesData;
 
 @Repository
 public interface NotesRepository extends JpaRepository<NotesData, Long> {
- 
+
 	void deleteByTitle(String title);
 
 	@Query(value = "UPDATE notes_data SET title = ?, description = ? where id = ?", nativeQuery = true)
 	void setTitleDescription(String editeTitle, String editeDescription, Long id);
+
+	@Query(value = "UPDATE notes_data SET is_trash = ? where id = ?", nativeQuery = true)
+	void setTrash(boolean setingTrashValue, long id);
+
+	@Query(value = "UPDATE notes_data SET is_pined = ? where id = ?", nativeQuery = true)
+	void setPined(boolean setingPinedValue, long id);
+
+	@Query(value = "UPDATE notes_data SET is_archive = ? where id = ?", nativeQuery = true)
+	void setArchive(boolean setingArchiveValue, long id);
+
+	@Query(value = "SELECT * FROM notes_data ORDER BY title", nativeQuery = true)
+	List<NotesData> sortByTitle();
+
+	@Query(value = "SELECT * FROM notes_data ORDER BY description", nativeQuery = true)
+	List<NotesData> sortByDescription();
+
+	@Query(value = "SELECT * FROM notes_data WHERE title = ?", nativeQuery = true)
+	List<NotesData> findByTitle(String title);
+
+	@Query(value = "SELECT * FROM notes_data WHERE description = ?", nativeQuery = true)
+	List<NotesData> findByDescription(String description);
 
 }
