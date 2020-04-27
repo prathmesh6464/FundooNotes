c2fa36ec01c61d89ae0c5f59e5bf10d630a95f7c
prathamesh <prathmesh6464@gmail.com>
2020-04-27 13:51:18 +0530

    REFACTORED : Added set trash untrash, set archive unarchive, set pined unpined, sort by title, sort by description, find by title, find by description methods.

--- FundooNotes/src/main/java/com/bridz/controller/NotesController.java
+++ FundooNotes/src/main/java/com/bridz/controller/NotesController.java
@@ -1,6 +1,5 @@
 package com.bridz.controller;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.bridz.dto.NotesDto;
-import com.bridz.model.NotesData;
 import com.bridz.service.NotesService;
 
 @RestController
@@ -35,18 +33,59 @@ public class NotesController {
 	}
 
 	@PutMapping("/updateNote/{id}")
-	public ResponseEntity<String> updateUserNote(@PathVariable("id") long id,
-			@RequestBody NotesDto notesDtoObject) {
+	public ResponseEntity<String> updateUserNote(@PathVariable("id") long id, @RequestBody NotesDto notesDtoObject) {
 
 		// Updating user note data into data base
 		return notesServiceObject.updateNote(notesDtoObject, id);
 	}
 
 	@GetMapping("/showNotes")
-	public List<NotesData> showeUserNotes() {
+	public ResponseEntity<Object> showeUserNotes() {
 
 		// Showing user note data from data base
 		return notesServiceObject.showNotes();
+	}
+
+	@PostMapping("/trashOrUnTrashNote/{id}")
+	public ResponseEntity<String> setTrashOrUntrash(@PathVariable("id") long id) {
+
+		return notesServiceObject.trashUntrash(id);
+	}
+
+	@PostMapping("/archiveOrUnArchiveNote/{id}")
+	public ResponseEntity<String> setArchiveOrUnArchive(@PathVariable("id") long id) {
+
+		return notesServiceObject.archiveUnArchive(id);
+	}
+
+	@PostMapping("/pinedOrUnPinedNote/{id}")
+	public ResponseEntity<String> setPinedOrUnPined(@PathVariable("id") long id) {
+
+		return notesServiceObject.pinedUnPined(id);
+	}
+
+	@GetMapping("/sortByTitle")
+	public ResponseEntity<Object> sortByTitle() {
+
+		return notesServiceObject.sortByTitle();
+	}
+
+	@GetMapping("/sortByDescription")
+	public ResponseEntity<Object> sortByDescription() {
+
+		return notesServiceObject.sortByDescription();
+	}
+
+	@GetMapping("/findByTitle/{title}")
+	public ResponseEntity<Object> findByTitle(@PathVariable("title") String title) {
+
+		return notesServiceObject.findByTitle(title);
+	}
+
+	@GetMapping("/findByDescription/{description}")
+	public ResponseEntity<Object> FindByDescription(@PathVariable("description") String description) {
+
+		return notesServiceObject.findByDescription(description);
 	}
 
 }
