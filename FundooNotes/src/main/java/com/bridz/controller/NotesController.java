f1000d59fd6b7f56583aef62480f9493c7d93d58
prathamesh <prathmesh6464@gmail.com>
2020-05-11 17:59:43 +0530

    REFACTORED : Added cross origin annotation, and changed variables name.

--- FundooNotes/src/main/java/com/bridz/controller/NotesController.java
+++ FundooNotes/src/main/java/com/bridz/controller/NotesController.java
@@ -2,64 +2,68 @@ package com.bridz.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.bridz.dto.NotesDto;
 import com.bridz.dto.ReminderDateTimeDto;
 import com.bridz.service.NotesService;
 
 @RestController
+@RequestMapping("/notes")
+@CrossOrigin(origins = "*")
 public class NotesController {
 
 	@Autowired
 	NotesService notesServiceObject;
 
-	@PostMapping("/addNote")
-	public ResponseEntity<String> addUserNote(@RequestBody NotesDto notesDtoObject) {
+	@PostMapping("/add")
+	public ResponseEntity<String> add(@RequestBody NotesDto notesDto) {
 
 		// Storing user note data into data base
-		return notesServiceObject.saveNote(notesDtoObject);
+		return notesServiceObject.save(notesDto);
 	}
 
-	@DeleteMapping("/deleteNote/{id}")
-	public ResponseEntity<String> deleteUserNote(@PathVariable("id") Long id) {
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> delete(@PathVariable("id") Long id) {
 
 		// Deleting user note from data base
-		return notesServiceObject.deleteNote(id);
+		return notesServiceObject.delete(id);
 	}
 
-	@PutMapping("/updateNote/{id}")
-	public ResponseEntity<String> updateUserNote(@PathVariable("id") long id, @RequestBody NotesDto notesDtoObject) {
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> update(@PathVariable("id") long id, @RequestBody NotesDto notesDto) {
 
 		// Updating user note data into data base
-		return notesServiceObject.updateNote(notesDtoObject, id);
+		return notesServiceObject.update(notesDto, id);
 	}
 
-	@GetMapping("/showNotes")
-	public ResponseEntity<Object> showeUserNotes() {
+	@GetMapping("/show")
+	public ResponseEntity<Object> show() {
 
 		// Showing user note data from data base
-		return notesServiceObject.showNotes();
+		return notesServiceObject.show();
 	}
 
-	@PostMapping("/trashOrUnTrashNote/{id}")
+	@PostMapping("/trashOrUnTrash/{id}")
 	public ResponseEntity<String> setTrashOrUntrash(@PathVariable("id") long id) {
 
 		return notesServiceObject.trashUntrash(id);
 	}
 
-	@PostMapping("/archiveOrUnArchiveNote/{id}")
+	@PostMapping("/archiveOrUnArchive/{id}")
 	public ResponseEntity<String> setArchiveOrUnArchive(@PathVariable("id") long id) {
 
 		return notesServiceObject.archiveUnArchive(id);
 	}
 
-	@PostMapping("/pinedOrUnPinedNote/{id}")
+	@PostMapping("/pinedOrUnPined/{id}")
 	public ResponseEntity<String> setPinedOrUnPined(@PathVariable("id") long id) {
 
 		return notesServiceObject.pinedUnPined(id);
@@ -90,10 +94,10 @@ public class NotesController {
 	}
 
 	@PostMapping("/setReminder/{id}")
-	public ResponseEntity<String> setReminder(@RequestBody ReminderDateTimeDto reminderDateTimeDtoObject,
+	public ResponseEntity<String> setReminder(@RequestBody ReminderDateTimeDto reminderDateTimeDto,
 			@PathVariable("id") long id) {
 
-		return notesServiceObject.setReminder(reminderDateTimeDtoObject, id);
+		return notesServiceObject.setReminder(reminderDateTimeDto, id);
 	}
 
 	@PostMapping("/unsetReminder/{id}")
@@ -103,10 +107,10 @@ public class NotesController {
 	}
 
 	@PostMapping("/resetReminder/{id}")
-	public ResponseEntity<String> resetReminder(@RequestBody ReminderDateTimeDto reminderDateTimeDtoObject,
+	public ResponseEntity<String> resetReminder(@RequestBody ReminderDateTimeDto reminderDateTimeDto,
 			@PathVariable("id") long id) {
 
-		return notesServiceObject.resetReminder(reminderDateTimeDtoObject, id);
+		return notesServiceObject.resetReminder(reminderDateTimeDto, id);
 	}
 
 }
