c894ca06825e6c3264a81e03a75b045c8cc8c8a3
prathamesh <prathmesh6464@gmail.com>
2020-05-18 04:12:33 +0530

    REFACTORED : Added jwt response token in parameter.

--- FundooNotes/src/main/java/com/bridz/service/LabelService.java
+++ FundooNotes/src/main/java/com/bridz/service/LabelService.java
@@ -1,16 +1,18 @@
 package com.bridz.service;
 
 import org.springframework.http.ResponseEntity;
+
+import com.bridz.dto.JwtResponseToken;
 import com.bridz.dto.LabelDto;
 
 public interface LabelService {
 
-	ResponseEntity<String> add(LabelDto labelDto);
+	ResponseEntity<String> add(LabelDto labelDto, JwtResponseToken token);
 
-	ResponseEntity<String> edite(LabelDto labelDto, long id);
+	ResponseEntity<String> edite(LabelDto labelDto, long id, JwtResponseToken token);
 
-	ResponseEntity<String> delete(long id);
+	ResponseEntity<String> delete(long id, JwtResponseToken token);
 
-	ResponseEntity<Object> show();
+	ResponseEntity<Object> show(JwtResponseToken token);	
 
 }
