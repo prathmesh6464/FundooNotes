6369241bf4f607146710ba6c1cea6d5a55631b73
prathamesh <prathmesh6464@gmail.com>
2020-05-18 04:00:45 +0530

    REFACTORED : Added token for user authentication.

--- FundooNotes/src/main/java/com/bridz/controller/NotesController.java
+++ FundooNotes/src/main/java/com/bridz/controller/NotesController.java
@@ -1,5 +1,6 @@
 package com.bridz.controller;
 
+import com.bridz.dto.JwtResponseToken;
 import com.bridz.dto.NotesDto;
 import com.bridz.dto.ReminderDateTimeDto;
 import com.bridz.service.NotesService;
@@ -17,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @RequestMapping("/notes")
 @CrossOrigin(origins = "*")
@@ -27,94 +26,98 @@ public class NotesController {
 	@Autowired
 	NotesService notesService;
 
-	@PostMapping("/add")
-	public ResponseEntity<String> add(@RequestBody NotesDto notesDto) {
+	@PostMapping("/add/{token}")
+	public ResponseEntity<String> add(@RequestBody NotesDto notesDto, @PathVariable JwtResponseToken token) {
 
 		// Storing user note data into data base
-		return notesService.save(notesDto);
+		return notesService.save(notesDto, token);
 	}
 
-	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<String> delete(@PathVariable("id") Long id) {
+	@DeleteMapping("/delete/{id}/{token}")
+	public ResponseEntity<String> delete(@PathVariable("id") Long id, @PathVariable JwtResponseToken token) {
 
 		// Deleting user note from data base
-		return notesService.delete(id);
+		return notesService.delete(id, token);
 	}
 
-	@PutMapping("/update/{id}")
-	public ResponseEntity<String> update(@PathVariable("id") long id, @RequestBody NotesDto notesDto) {
+	@PutMapping("/update/{id}/{token}")
+	public ResponseEntity<String> update(@PathVariable("id") long id, @RequestBody NotesDto notesDto,
+			@PathVariable JwtResponseToken token) {
 
 		// Updating user note data into data base
-		return notesService.update(notesDto, id);
+		return notesService.update(notesDto, id, token);
 	}
 
-	@GetMapping("/show")
-	public ResponseEntity<Object> show() {
+	@GetMapping("/show/{token}")
+	public ResponseEntity<Object> show(@PathVariable JwtResponseToken token) {
 
 		// Showing user note data from data base
-		return notesService.show();
+		return notesService.show(token);
 	}
 
-	@PutMapping("/trashOrUnTrash/{id}")
-	public ResponseEntity<String> setTrashOrUntrash(@PathVariable("id") long id) {
+	@PutMapping("/trashOrUnTrash/{id}/{token}")
+	public ResponseEntity<String> setTrashOrUntrash(@PathVariable("id") long id, @PathVariable JwtResponseToken token) {
 
-		return notesService.trashUntrash(id);
+		return notesService.trashUntrash(id, token);
 	}
 
-	@PutMapping("/archiveOrUnArchive/{id}")
-	public ResponseEntity<String> setArchiveOrUnArchive(@PathVariable("id") long id) {
+	@PutMapping("/archiveOrUnArchive/{id}/{token}")
+	public ResponseEntity<String> setArchiveOrUnArchive(@PathVariable("id") long id,
+			@PathVariable JwtResponseToken token) {
 
-		return notesService.archiveUnArchive(id);
+		return notesService.archiveUnArchive(id, token);
 	}
 
-	@PutMapping("/pinedOrUnPined/{id}")
-	public ResponseEntity<String> setPinedOrUnPined(@PathVariable("id") long id) {
+	@PutMapping("/pinedOrUnPined/{id}/{token}")
+	public ResponseEntity<String> setPinedOrUnPined(@PathVariable("id") long id, @PathVariable JwtResponseToken token) {
 
-		return notesService.pinedUnPined(id);
+		return notesService.pinedUnPined(id, token);
 	}
 
-	@GetMapping("/sortByTitle")
-	public ResponseEntity<Object> sortByTitle() {
+	@GetMapping("/sortByTitle/{token}")
+	public ResponseEntity<Object> sortByTitle(@PathVariable JwtResponseToken token) {
 
-		return notesService.sortByTitle();
+		return notesService.sortByTitle(token);
 	}
 
-	@GetMapping("/sortByDescription")
-	public ResponseEntity<Object> sortByDescription() {
+	@GetMapping("/sortByDescription/{token}")
+	public ResponseEntity<Object> sortByDescription(@PathVariable JwtResponseToken token) {
 
-		return notesService.sortByDescription();
+		return notesService.sortByDescription(token);
 	}
 
-	@GetMapping("/findByTitle/{title}")
-	public ResponseEntity<Object> findByTitle(@PathVariable("title") String title) {
+	@GetMapping("/findByTitle/{title}/{token}")
+	public ResponseEntity<Object> findByTitle(@PathVariable("title") String title,
+			@PathVariable JwtResponseToken token) {
 
-		return notesService.findByTitle(title);
+		return notesService.findByTitle(title, token);
 	}
 
-	@GetMapping("/findByDescription/{description}")
-	public ResponseEntity<Object> FindByDescription(@PathVariable("description") String description) {
+	@GetMapping("/findByDescription/{description}/{token}")
+	public ResponseEntity<Object> FindByDescription(@PathVariable("description") String description,
+			@PathVariable JwtResponseToken token) {
 
-		return notesService.findByDescription(description);
+		return notesService.findByDescription(description, token);
 	}
 
-	@PostMapping("/setReminder/{id}")
+	@PostMapping("/setReminder/{id}/{token}")
 	public ResponseEntity<String> setReminder(@RequestBody ReminderDateTimeDto reminderDateTimeDto,
-			@PathVariable("id") long id) {
+			@PathVariable("id") long id, @PathVariable JwtResponseToken token) {
 
-		return notesService.setReminder(reminderDateTimeDto, id);
+		return notesService.setReminder(reminderDateTimeDto, id, token);
 	}
 
-	@PutMapping("/unsetReminder/{id}")
-	public ResponseEntity<String> unsetReminder(@PathVariable("id") long id) {
+	@PutMapping("/unsetReminder/{id}/{token}")
+	public ResponseEntity<String> unsetReminder(@PathVariable("id") long id, @PathVariable JwtResponseToken token) {
 
-		return notesService.unsetReminder(id);
+		return notesService.unsetReminder(id, token);
 	}
 
-	@PutMapping("/resetReminder/{id}")
+	@PutMapping("/resetReminder/{id}/{token}")
 	public ResponseEntity<String> resetReminder(@RequestBody ReminderDateTimeDto reminderDateTimeDto,
-			@PathVariable("id") long id) {
+			@PathVariable("id") long id, @PathVariable JwtResponseToken token) {
 
-		return notesService.resetReminder(reminderDateTimeDto, id);
+		return notesService.resetReminder(reminderDateTimeDto, id, token);
 	}
 
 }
