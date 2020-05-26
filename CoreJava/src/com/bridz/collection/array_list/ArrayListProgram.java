79ba68249fab25d5e34336e6400a43be70b8a532
prathamesh <prathmesh6464@gmail.com>
2020-05-26 21:34:23 +0530

    DELETED : Core java project.

--- CoreJava/src/com/bridz/collection/array_list/ArrayListProgram.java
+++
@@ -1,8 +0,0 @@
-package com.bridz.collection.array_list;
-
-public class ArrayListProgram {
-	public static void main(String args[]) {
-		System.out.println("hiii");
-	}
-
-}
--- CoreJava/src/com/bridz/collection/linked_list/LinkedList.java
+++
@@ -1,7 +0,0 @@
-package com.bridz.collection.linked_list;
-
-public class LinkedList {
-	public static void main(String args[]) {
-		System.out.println("link");
-	}
-}
