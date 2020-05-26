346726273460b67ad88a471a2548647e9c328768
prathamesh <prathmesh6464@gmail.com>
2020-05-26 21:33:31 +0530

    ADDED : Core java project.

---
+++ CoreJava/src/com/bridz/collection/array_list/ArrayListProgram.java
@@ -0,0 +1,8 @@
+package com.bridz.collection.array_list;
+
+public class ArrayListProgram {
+	public static void main(String args[]) {
+		System.out.println("hiii");
+	}
+
+}
---
+++ CoreJava/src/com/bridz/collection/linked_list/LinkedList.java
@@ -0,0 +1,7 @@
+package com.bridz.collection.linked_list;
+
+public class LinkedList {
+	public static void main(String args[]) {
+		System.out.println("link");
+	}
+}
